723e19080da20715a2ea826358e9a2e59551790d
ivan_kzkv <152160159+ivan-kzkv@users.noreply.github.com>
2024-06-25 16:59:32 +0300

    [TCX-2] add migrations
    * v1.0.1
    
    * [tcx]-2 add migrations instrument and create scenario migration

---
+++ service/src/main/resources/db/migration/V1.0.1__Create_scenario_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS scenario (
+
+                                          id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                          name varchar(32),
+                                          date_created timestamp,
+                                          date_modified timestamp
+
+);
