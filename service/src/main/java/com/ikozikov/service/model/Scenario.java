747d364e4ed14c6ffbec551edcb3562ba9dd8f29
ivan_kzkv <152160159+ivan-kzkv@users.noreply.github.com>
2024-06-27 09:53:24 +0300

    [TCX-3] make mvc branch for scenario (#2)

---
+++ service/src/main/java/com/ikozikov/service/model/Scenario.java
@@ -0,0 +1,29 @@
+package com.ikozikov.service.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Scenario {
+  
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String name;
+  @Column(updatable = false)
+  private Date date_created;
+  
+  private Date date_modified;
+
+}
