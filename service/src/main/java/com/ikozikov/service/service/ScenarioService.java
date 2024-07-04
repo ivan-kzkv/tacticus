4930f8ee98737cbe18f7a53a634f6a178020502b
Ivan Kozikov <kozikov.work@gmail.com>
2024-07-04 19:35:02 +0300

    [TCX-9] fill ScenarioService.java with crud methods

--- service/src/main/java/com/ikozikov/service/service/ScenarioService.java
+++ service/src/main/java/com/ikozikov/service/service/ScenarioService.java
@@ -5,6 +5,7 @@ import com.ikozikov.service.repository.ScenarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,5 +15,30 @@ public class ScenarioService {
   
   public List<Scenario> listAllScenarios() {
     return this.scenarioRepository.findAll();
+  }
+  
+  public Scenario getOneScenario(Long scenarioId) {
+    return this.scenarioRepository.findById(scenarioId).orElseThrow();
+  }
+  
+  public Scenario createScenario(Scenario scenario) {
+    return this.scenarioRepository.save(scenario);
+  }
+  
+  public List<Scenario> createScenarioFromList(List<Scenario> scenarios) {
+    return this.scenarioRepository.saveAll(scenarios);
+  }
+  
+  public Scenario updateScenario(Scenario newScenario) {
+    return this.scenarioRepository.findById(newScenario.getId())
+        .map(scenario -> {
+          scenario.setName(newScenario.getName());
+          scenario.setDate_modified(new Date());
+          return this.scenarioRepository.save(scenario);
+        }).orElseThrow();
+  }
+  
+  public void deleteScenario(List<Scenario> scenarios) {
+    this.scenarioRepository.deleteAll(scenarios);
   }
 }
