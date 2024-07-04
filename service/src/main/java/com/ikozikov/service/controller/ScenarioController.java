8d41304068e28ddf3234c908697e3f120b04091f
Ivan Kozikov <kozikov.work@gmail.com>
2024-07-04 19:47:04 +0300

    [TCX-9] fill ScenarioController.java

--- service/src/main/java/com/ikozikov/service/controller/ScenarioController.java
+++ service/src/main/java/com/ikozikov/service/controller/ScenarioController.java
@@ -3,7 +3,12 @@ package com.ikozikov.service.controller;
 import com.ikozikov.service.model.Scenario;
 import com.ikozikov.service.service.ScenarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,4 +23,26 @@ public class ScenarioController {
   public List<Scenario> getAllScenarios() {
     return this.scenarioService.listAllScenarios();
   }
+  
+  @GetMapping(value = "/scenario/{id}")
+  public Scenario getScenarioById(@PathVariable(value = "id") Long scenarioId) {
+    return this.scenarioService.getOneScenario(scenarioId);
+  }
+  
+  @PostMapping(value = "/scenario")
+  public Scenario createScenario(@RequestBody Scenario newScenario) {
+    return this.scenarioService.createScenario(newScenario);
+  }
+  
+  @PatchMapping(value = "/scenario/{id}")
+  public Scenario updateScenario(@PathVariable(value = "id") Long scenarioId, @RequestBody Scenario newScenario) {
+    return this.scenarioService.updateScenario(scenarioId, newScenario);
+  }
+  
+  @DeleteMapping(value = "/scenario")
+  public void deleteScenarios(@RequestBody List<Scenario> scenarios) {
+    this.scenarioService.deleteScenario(scenarios);
+  }
+  
+  
 }
