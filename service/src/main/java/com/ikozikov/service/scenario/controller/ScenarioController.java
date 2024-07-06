5bb7e15f086df07707acb8e689a842739bbbeecb
Ivan Kozikov <kozikov.work@gmail.com>
2024-07-06 15:56:41 +0300

    [TCX-9] remove unused imports in ScenarioController.java

--- service/src/main/java/com/ikozikov/service/scenario/controller/ScenarioController.java
+++ service/src/main/java/com/ikozikov/service/scenario/controller/ScenarioController.java
@@ -1,7 +1,6 @@
 package com.ikozikov.service.scenario.controller;
 
 import com.ikozikov.service.scenario.dto.ScenarioDto;
-import com.ikozikov.service.scenario.model.Scenario;
 import com.ikozikov.service.scenario.service.ScenarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
